16ddbf49edd2b5a930986bbbe0321b10feb0559e
Swapnil Bhatnagar <132487560+Swapnil223355@users.noreply.github.com>
2024-01-11 14:54:35 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{Your API Key Here}",
+  base: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1f4fd1f2b58d2a66cde7ad45d9eef50e",
+};
